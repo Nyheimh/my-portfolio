0776c153792371a1d2cb9e4b9c84a1684728a8a5
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2024-03-07 14:34:16 -0800

    Fixed cards, time, and responses

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,11 +8,9 @@ const Card = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="mx-auto max-w-screen-lg px-4 py-8">
-      <div className="w-290 h-64 rounded-lg bg-gray-200 p-8  shadow-lg md:w-96">
-        <div className="text-l mb-4 font-bold">{label}</div>
-        <div>{children}</div>
-      </div>
+    <div className="mx-auto h-56 w-full max-w-sm rounded-lg bg-zinc-200  p-8 shadow-lg md:w-96">
+      <div className="mb-4 text-lg font-bold">{label}</div>
+      <div>{children}</div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,123 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client";
+import React, { useState, useEffect } from "react";
 import Headers from "./components/Headers";
 import Card from "./components/Card";
 
-type Weather = {
-  latitude: number;
-  longitude: number;
-  generationtime_ms: number;
-  utc_offset_seconds: number;
-  timezone: string;
-  timezone_abbreviation: string;
-  elevation: number;
-  current_units: {
-    time: string;
-    interval: string;
-    temperature_2m: string;
-    wind_speed_10m: string;
-    relative_humidity_2m: string;
-  };
+interface WeatherData {
   current: {
-    time: string;
-    interval: number;
-    temperature_2m: number;
-    wind_speed_10m: number;
-    relative_humidity_2m: number;
+    temperature_2m?: number;
+    wind_speed_10m?: number;
+    relative_humidity_2m?: number;
   };
-};
+}
 
-type CatFacts = {
-  data: string[];
-};
+interface CatFacts {
+  data?: string[];
+}
 
-export default async function HomePage() {
-  const now = new Date();
-  const currentDateTime = now.toLocaleString("en-US", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
-  });
+const HomePage = () => {
+  const [currentDateTime, setCurrentDateTime] = useState("");
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [catData, setCatData] = useState<CatFacts | null>(null);
 
-  const weatherData = await getWeather();
-  const catData = await getCatFacts();
+  const updateDateTime = () => {
+    const now = new Date();
+    const newDateTime = now.toLocaleString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    setCurrentDateTime(newDateTime);
+  };
+
+  useEffect(() => {
+    updateDateTime();
+    const timerId = setInterval(updateDateTime, 60000);
+    return () => clearInterval(timerId);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const weatherResponse = await fetch(
+          "https://api.open-meteo.com/v1/forecast?latitude=41.655300&longitude=-83.535721&current=temperature_2m,wind_speed_10m,relative_humidity_2m",
+        );
+
+        if (!weatherResponse.ok) {
+          throw new Error("Failed to fetch weather data");
+        }
+
+        const weatherData: WeatherData = await weatherResponse.json();
+        setWeatherData(weatherData);
+
+        const catFactsResponse = await fetch(
+          "https://meowfacts.herokuapp.com/",
+        );
+
+        if (!catFactsResponse.ok) {
+          throw new Error("Failed to fetch cat facts data");
+        }
+
+        const catFactsData: CatFacts = await catFactsResponse.json();
+        setCatData(catFactsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData().catch((error) => console.error("Error in fetchData:", error));
+  }, []);
+
+  if (!weatherData || !catData) return <div>Loading Responses...</div>;
 
   return (
     <main>
       <Headers />
-      <div className=" mx-auto grid max-w-screen-lg grid-cols-1 items-center justify-center p-6  md:grid-cols-2">
-        <div className="mb-8 mt-20 h-64 w-96">
-          <h1 className=" p-6 text-5xl font-bold">It's {currentDateTime}</h1>
-        </div>
-        <div className="w-full p-2">
-          <Card label="About me">
-            I enjoy outdoors 🧗🏽‍♀️, trying new food 🧆, traveling ⛰️, video games
-            👾, sports 🏈🏃🏾‍♂️, and new technology 🖥️. I enjoy visiting new places
-            and learning about different cultures.
-          </Card>
-        </div>
-        <div className="w-1/2 p-2">
-          <Card label="Sacramento, CA">
-            <p className="mb-4 text-4xl font-bold">
-              {weatherData.current.temperature_2m}° F
-            </p>
-            <p className="font-bold">
-              {" "}
-              Wind speed {weatherData.current.wind_speed_10m} km/h
-            </p>
-            <p className="font-bold">
-              Humidity {weatherData.current.relative_humidity_2m}%
-            </p>
-          </Card>
-        </div>
-        <div className="w-1/2 p-2">
-          <Card label="Cat Fact">
-            <p>{catData.data}</p>
-          </Card>
+      <div className="flex h-screen items-center justify-center">
+        <div className="mx-auto grid max-w-screen-lg grid-cols-1 gap-6 p-6 md:grid-cols-2 md:p-8 lg:gap-8">
+          <div className="h-64 w-full text-center text-5xl font-bold md:w-96">
+            <h1>It's {currentDateTime}</h1>
+          </div>
+          <div className=" w-full p-2 md:w-1/2">
+            <Card label="About me">
+              <p className="text-sm">
+                I enjoy outdoors 🧗🏽‍♀️, trying new food 🧆, traveling ⛰️, video
+                games 👾, and sports 🏈🏃🏾‍♂️. I enjoy visiting new places and
+                learning about different cultures.
+              </p>
+              <p className="pt-2 text-sm">
+                My top programming languages are Javascript, React, Next, Go,
+                and Nest.
+              </p>
+            </Card>
+          </div>
+          <div className="w-full p-2 md:w-1/2">
+            <Card label="Sacramento, CA">
+              <p className="mb-4 text-4xl font-bold">
+                {weatherData?.current?.temperature_2m ?? "N/A"}° F
+              </p>
+              <p className="font-bold">
+                Wind speed {weatherData?.current.wind_speed_10m ?? "N/A"} km/h
+              </p>
+              <p className="font-bold">
+                Humidity {weatherData?.current.relative_humidity_2m}%
+              </p>
+            </Card>
+          </div>
+          <div className="w-full p-2 md:w-1/2">
+            <Card label="Cat Fact">
+              <p className="text-sm">{catData?.data}</p>
+            </Card>
+          </div>
         </div>
       </div>
     </main>
   );
-}
+};
 
-async function getWeather(): Promise<Weather> {
-  const data = await fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=41.655300&longitude=-83.535721&current=temperature_2m,wind_speed_10m,relative_humidity_2m",
-  );
-  return data.json() as Promise<Weather>;
-}
-
-async function getCatFacts(): Promise<CatFacts> {
-  const data = await fetch("https://meowfacts.herokuapp.com/");
-  return data.json() as Promise<CatFacts>;
-}
+export default HomePage;
